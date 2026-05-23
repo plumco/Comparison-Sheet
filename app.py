b356faedf7d4b711c72d22ce9fa5ad09a6b85e39
Umesh Nikam <50052472+plumco@users.noreply.github.com>
2026-05-23 23:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ with open("index.html", "r", encoding="utf-8") as f:
     html_data = f.read()
 
 # Render the HTML (I increased the height here so it fits better)
-components.html(html_data, height=1000, scrolling=True)
+components.html(html_data, height=1300, scrolling=True)
