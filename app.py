c85c341a9914c086691cbd4fff52519bdc61f98c
Umesh Nikam <50052472+plumco@users.noreply.github.com>
2026-05-23 23:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ with open("index.html", "r", encoding="utf-8") as f:
     html_data = f.read()
 
 # Render the HTML inside your Streamlit app
-components.html(html_data, height=800, scrolling=True)
+components.html(html_data, height=1600, scrolling=True)
