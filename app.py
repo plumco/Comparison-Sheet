59eed074f00167b841e604731e952ec3b6fbf5cc
Umesh Nikam <50052472+plumco@users.noreply.github.com>
2026-05-23 23:19:55 +0530

    Modify HTML rendering settings in app.py
    Updated encoding to 'utf-12' and changed height to 800.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Open your HTML file and read it
-with open("index.html", "r", encoding="utf-8") as f:
+with open("index.html", "r", encoding="utf-12") as f:
     html_data = f.read()
 
 # Render the HTML inside your Streamlit app
-components.html(html_data, height=1600, scrolling=True)
+components.html(html_data, height=800, scrolling=True)
